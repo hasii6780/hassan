046a7d92d488d27000a1fda4da0b7101ed94ad32
hasii6780 <hassanitx524@gmail.com>
2025-01-04 20:46:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "OdpkWaST#QLU8rsI6ZDVvxOjCW4LoM_d2eiKnHYYJQiY7uJMOU2U",
     CAPTION: process.env.CAPTION || "POWERED BY HASSAN-RAJPOOT214",
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
